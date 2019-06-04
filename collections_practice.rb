449cec8048683525595eb39a01111fe01f437e25
Dan-Vy Le <danvyle85@gmail.com>
2019-06-04 05:35:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,10 +44,11 @@ end
 def add_s(array)
   # new_array = []
     array.collect.each_with_index do |element, index|
-    if index != 1 
-      element.to_s << "s"
-    else
-      element
+      if index != 1 
+       element.to_s << "s"
+      else
+       element
+      end
     end
     array
 end
